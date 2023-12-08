b65c4bb5bf920e0c42db09ee4cdd0e8b938d9cdf
Heneros <punk60826@gmail.com>
2023-12-08 14:29:34 +0200

    textQuest.json

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const fs = require('fs')
+const express = require('express');
 
+const app = express();
 
-fs.readFile("./assets/js/data/gameData.json", "utf8", (error, data) => {
-    if (error) {
-        console.log(error);
-        return;
+app.use(express.static('./public'));
+app.use(express.json());
+
+const port = 4000;
+const start = async () => {
+    try {
+        app.listen(port, console.log(`Working on port ${port} `));
+    } catch (error) {
+        console.log(error)
     }
-    console.log(JSON.parse(data));
-})
+}
+
+start();
+
+// fs.readFile("./assets/js/data/gameData.json", "utf8", (error, data) => {
+//     if (error) {
+//         console.log(error);
+//         return;
+//     }
+//     console.log(JSON.parse(data));
+// })
