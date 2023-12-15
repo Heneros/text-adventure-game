2e25bea703c2a3e2ddb247e92d3ebddd1101c7e7
Heneros <punk60826@gmail.com>
2023-12-16 00:59:33 +0200

    ...

--- index.js
+++ index.js
@@ -3,17 +3,35 @@ const express = require('express');
 
 const app = express();
 
+
+
+app.use(express.static(__dirname + '/public'));
+
 app.use(express.static('./views'));
 app.use(express.json());
 app.set('view engine', 'ejs');
 
 
 
-app.get('/', (req, res) => {
-    const data = JSON.parse(fs.readFileSync('./data/textQuest.json'))
-   // storyData  <-- it's key
-    res.render('index', { storyData: data });
+// app.get('/', (req, res) => {
+//     const data = JSON.parse(fs.readFileSync('./data/textQuest.json'))
+//     // storyData  <-- it's key
+//     res.render('index', { storyData: data });
+// });
+
+
+
+let data = JSON.parse(fs.readFileSync('./data/textQuest.json'))
+
+app.get('/:id', (req, res) => {
+    let storyItem = data.find(item => item.id == req.params.id);
+    if (!storyItem) {
+        res.status(404).send('Not found');
+        return;
+    }
+    res.render('index', { storyItem: storyItem });
 });
+
 
 const port = 4000;
 const start = async () => {
